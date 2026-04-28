cf8f7e2e03df460158bdb9b1069b560b7f262fa2
Ayush Bommana <bommanaayush07@gmail.com>
2026-04-28 14:22:51 +0530

    feat: Add poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider"
+import { Poppins } from "next/font/google";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // choose what you want
+  variable: "--font-poppins", // this becomes CSS variable
 });
 
 export const metadata: Metadata = {
@@ -30,7 +24,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className="dark"
+      className={`dark ${poppins.className}`}
     >
       <body>
         <ThemeProvider
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import {Button} from "@/components/ui/button";
 
 const page = () => {
-  return (
-    <div>
-      <Button>Hi</Button>
-    </div>
-  );
+    return (
+        <div>
+            <Button>Hi</Button>
+            <h1>HEY</h1>
+        </div>
+    );
 }
 
 export default page;
