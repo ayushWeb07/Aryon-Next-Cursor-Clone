e38576d8c0b737b9c0bb690b0e0304e733083313
Ayush Bommana <bommanaayush07@gmail.com>
2026-05-05 01:05:20 +0530

    feat: Setup clerk for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Outfit } from "next/font/google";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -31,7 +33,23 @@ export default function RootLayout({
             defaultTheme="dark"
             disableTransitionOnChange
         >
-          {children}
+          <ClerkProvider>
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <Show when="signed-out">
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+
+            {children}
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
